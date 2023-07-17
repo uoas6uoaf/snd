f80dc71eba4ca4270f73b13407e91af5a02c14e1
uoas6uoaf <uoas6uoaf@gmail.com>
2023-07-18 01:44:55 +0900

    푸터 변경

--- inc/footer.php
+++ inc/footer.php
@@ -1,25 +1,21 @@
 
 <footer class="footer">
+    <div class="com_info">
+        <img src="./img/logo2.png">
+        <ul>
+            <li>회사명 &#58; 동방에스앤디</li>
+            <li>주소 &#58; 서울 중구 소공로 88, 701호</li>
+            <li>TEL &#58; 02-2190-8400</li>
+            <li>사업자번호 &#58; 123-456-789</li>
+        </ul>
+    </div>
+    <div class="line"></div>
     <div class="copyright">
-        <div class="inner">
-            <ul>
-                <li><a href="#">개인정보처리방침</a> </li>
-                <li>ㆍ</li>
-                <li><a href="#">공정거래자율준수</a> </li>
-                <li>ㆍ</li>
-                <li><a href="#">영상정보 처리기기 설치 운영 방침</a> </li>
-                <li>ㆍ</li>
-                <li><a href="#">찾아오시는 길</a> </li>
-                <li>
-                    <button type="button" onclick="location.href='menu01_06.php'">계열사 바로가기</button>
-                </li>
-            </ul>
-            <div>
-                <p>100-700 서울특별시 중구 남대문로 63(소공동) 한진빌딩 신관 7층 TEL (02)2190-8400
-                    <br>Copyright(C) 2021 DongbangSystem, All right reserved.
-                </p>
-            </div>
-        </div>
-
+        <ul>
+            <li>이용약관</li>
+            <li>개인정보처리방침</li>
+            <li>이메일무단수집거부</li>
+        </ul>
+        <div>Copyright © 2014 dongbangsystem rights reserved.</div>
     </div>
 </footer>
